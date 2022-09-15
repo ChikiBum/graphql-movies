7958cd707e9a21e6598391a615dfeb40be3050d9
Oleksandr Bozhko <obozhko82@gmail.com>
2022-09-15 22:33:49 +0300

    Add new branch

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -19,6 +19,75 @@ const movie = {
 };
 
 const Home = () => {
+  const loadGrain = (arr: any) => {
+    if (arr.length < 3) {
+      return 0;
+    }
+
+    const lastIndex = arr.length - 1;
+    const firstValue = arr[0];
+
+    const even = (el: any) => el === firstValue;
+
+    if (arr.every(even)) {
+      return 0;
+    }
+
+    if (arr.length === 3 && arr[1] > firstValue) {
+      return 0;
+    }
+
+    let prevBlockHeight = 0;
+    let prevBlockIndex = 0;
+    let candidate = 0;
+
+    return arr.reduce(function (
+      accumulator: number,
+      currentValue: number,
+      index: number,
+      array: any
+    ) {
+      if (index === 0) {
+        prevBlockHeight = currentValue;
+      }
+      if (prevBlockHeight > currentValue) {
+        candidate = candidate + (prevBlockHeight - currentValue);
+      }
+      if (prevBlockHeight < currentValue) {
+        accumulator = accumulator + candidate;
+        prevBlockHeight = currentValue;
+        prevBlockIndex = index;
+        candidate = 0;
+      }
+      if (index === lastIndex) {
+        if (currentValue < prevBlockHeight) {
+          accumulator =
+            accumulator +
+            candidate -
+            (prevBlockHeight - currentValue) * (lastIndex - prevBlockIndex);
+        } else {
+          accumulator = accumulator + candidate;
+        }
+      }
+      return accumulator;
+    },
+    0);
+  };
+  console.log("loadGrain([4, 1, 3]): ", loadGrain([4, 1, 3]));
+  console.log(
+    "loadGrain([2, 1, 5, 2, 7, 4, 10]): ",
+    loadGrain([2, 1, 5, 2, 7, 4, 10])
+  );
+  console.log("loadGrain([2, 0, 1, 5, 2, 7]): ", loadGrain([2, 0, 1, 5, 2, 7]));
+  console.log("loadGrain([2, 4, 2]): ", loadGrain([2, 4, 2]));
+
+  console.log("loadGrain([8, 1, 3, 2]): ", loadGrain([8, 1, 3, 2]));
+  console.log(
+    "loadGrain([4, 0, 1, 2, 0, 0, 3, 0, 1, 2]): ",
+    loadGrain([4, 0, 1, 2, 0, 0, 3, 0, 1, 2])
+  );
+  console.log("loadGrain([4, 0, 1, 3]): ", loadGrain([4, 0, 1, 3]));
+
   return (
     <Box sx={{ flexGrow: 1, marginTop: 2 }}>
       <Grid container spacing={2}>
